f5d067bdb53b786bbf15961ae24259e9a67d2cd6
Jenn Tustin <jenn.tustin@gmail.com>
2016-10-12 22:12:48 -0400

    extractor.rb passing, first 2 of separator.rb passing

--- lib/extractor.rb
+++ lib/extractor.rb
@@ -1,2 +1,18 @@
 class Extractor
+
+  def phone_numbers(text)
+    only_phone_numbers_with_symbols = /\(\d{3}\) \d{3}\-\d{4}/
+    phone_numbers = text.scan(only_phone_numbers_with_symbols)
+  end
+
+# locations.map do |location|
+#   city_state = /([\w\s-]+), ([A-Z]{2})/.match(location)
+
+# expect(@x.emails(text)).to eq ['amaranth@gmail.com',
+#                                'pea@sprouts.org',
+#                                'cucumber.earthnut@pea.net']
+  def emails(text)
+    emails = /\w+\.\w+\@\w+\.\w*|\w+\@\w+\.\w*/
+    text.scan(emails)
+  end
 end
--- lib/separator.rb
+++ lib/separator.rb
@@ -1,2 +1,53 @@
 class Separator
+  def words(string)
+    if string.scan(/^\d.$/) == [string]
+      # (/^\d.$/).match(string)
+      return nil
+    else
+      string.scan(/\S+/)
+    end
+  end
+
+  def phone_number(number)
+    match = /\(?(\d{3})\)?[ -\.]?(\d{3})[ -\.]?(\d{4})/.match(number)
+
+    if match.nil?
+      return nil
+    end
+
+    {'area_code': match[1], 'number': "#{match[2]}-#{match[3]}"}
+  end
+
+  def money(amount)
+    separated_amount = /(\$)(\d*[,]?[.]?\d+)/.match(amount)
+
+    {'currency': separated_amount[1] , 'amount': separated_amount[2].to_f }
+  end
+  #(\$)(\d*) gets $4, $19, and $19.00
+  #<%= number_with_precision(@number, :precision => 2, :delimiter => ',') %>
+# $4
+# $19
+# $19.00
+# $3.58
+# $1000
+# $1000.00
+# $1,000
+# $1,000.00
+# $5,555,555
+# $5,555,555.55
+# $45,555,555.55
+# $456,555,555.55
+# $1234567.89
+#
+# $12,34
+# $1234.9
+# $1234.999
+# $
+# 31
+# $$31
+  def zipcode(code)
+
+  end
+# {'zip': '63936', 'plus4': nil}
+
 end
